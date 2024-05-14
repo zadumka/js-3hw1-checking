34dc8c7fdff37748c9f3a723b309ceb9be5d4a43
zadumka <alenahas1987@gmail.com>
2024-05-14 14:43:52 +0300

    add c

---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+function filterArray(numbers, value) {
+  const filteredArray = [];
+  numbers.forEach(elem => {
+    elem > value ? filteredArray.push(elem) : null;
+  });
+  return filteredArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
